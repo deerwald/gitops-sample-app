1f971da38127fba3af62b65566628a31a012382a
Andrew Hromis <ahromis@gmail.com>
2024-06-13 14:15:55 -0400

    more testing

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	// Write the response to the client
-	fmt.Fprint(w, "Hello, world!")
+	fmt.Fprint(w, "Hello, race fans!")
 }
 
 func main() {
